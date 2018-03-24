429865197b3cfbe5b1a632cb8a5d595be92eac7b
Ana Czarnitzki <aczarnitzki@medallia.com>
2018-03-24 13:11:02 -0300

    Selection and insertion sort added

---
+++ sorting.py
@@ -0,0 +1,27 @@
+def maximum(l, n):
+	max_index = 0
+
+	for x in range(1, n+1):
+		if (l[x] > l[max_index]):
+			max_index = x
+
+	return max_index 
+
+def swap(l, n1, n2):
+	l[n1], l[n2] = l[n2], l[n1]
+
+def insertion_sort(l):
+	n = len(l) - 1
+	while n > 0:
+		max_index = maximum(l, n)
+		swap(l, n, max_index)
+		n -= 1
+
+
+def selection_sort(l):
+	for i in range(len(l)):
+		j = i - 1
+		while (j >= 0 and l[j] < l[i]):
+			j -= 1
+		if (l[j] < l[i]):
+			swap(l, i, j) 
