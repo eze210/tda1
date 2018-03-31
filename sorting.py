fa54a8def88d129cd9ff65814b8525c837d5eef0
axl <voices117@gmail.com>
2018-03-31 01:11:20 -0300

    se arregla insertion sort y se hacen 'privadas' las funciones internas

--- sorting.py
+++ sorting.py
@@ -1,33 +1,33 @@
-import heap 
+import heap
 
-def maximum(l, n):
+def _maximum(l, n):
 	max_index = 0
 
 	for x in range(1, n+1):
 		if (l[x] > l[max_index]):
 			max_index = x
 
-	return max_index 
+	return max_index
 
-def swap(l, n1, n2):
+def _swap(l, n1, n2):
 	l[n1], l[n2] = l[n2], l[n1]
 
 def selection_sort(l):
 	n = len(l) - 1
 	while n > 0:
-		max_index = maximum(l, n)
-		swap(l, n, max_index)
+		max_index = _maximum(l, n)
+		_swap(l, n, max_index)
 		n -= 1
 
 def insertion_sort(l):
-	for i in range(len(l)):
+	for i, e in enumerate(l):
 		j = i - 1
-		while (j >= 0 and l[j] < l[i]):
+		while j >= 0 and l[j] > e:
+			l[j+1] = l[j]
 			j -= 1
-		if (l[j] < l[i]):
-			swap(l, i, j)
+		l[j+1] = e
 
-def merge(l1, l2):
+def _merge(l1, l2):
 	l = []
 	i = 0
 	j = 0
@@ -39,7 +39,7 @@ def merge(l1, l2):
 		else:
 			l.append(l2[j])
 			j += 1
-	
+
 	return l + l1[i:] + l2[j:] #syntactic sugar
 
 def mergesort(l):
@@ -48,7 +48,7 @@ def mergesort(l):
 	m = len(l)//2
 	left = mergesort(l[:m])
 	right = mergesort(l[m:])
-	return merge(left, right)
+	return _merge(left, right)
 
 def quicksort(l):
 	_quicksort(l, 0, len(l) - 1)
@@ -64,15 +64,15 @@ def _quicksort(l, start, end):
 		if (l[x] < l[pivot]):
 			start_lesser += 1
 			if (x != start_lesser):
-				swap(l, x, start_lesser)
+				_swap(l, x, start_lesser)
 
 	if (pivot != start_lesser):
-		swap(l, pivot, start_lesser)
+		_swap(l, pivot, start_lesser)
 
 	_quicksort(l, start, start_lesser - 1)
 	_quicksort(l, start_lesser + 1, end)
 
-def comp(elem1, elem2):
+def _comp(elem1, elem2):
 	if elem1 > elem2:
 		return -1
 	elif elem1 == elem2:
@@ -81,7 +81,7 @@ def comp(elem1, elem2):
 		return 1
 
 def heapsort(l):
-	heap.MaxHeap(comp, l).heapsort()
+	heap.MaxHeap(_comp, l).heapsort()
 	return l
 
 if __name__ == '__main__':
