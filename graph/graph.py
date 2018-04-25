4109ca67ae4a88a00beaac22651cfa5041095be8
eze210 <eze210@gmail.com>
2018-04-25 18:22:15 -0300

    Adding Dijkstra as a Functor.

--- graph/graph.py
+++ graph/graph.py
@@ -1,6 +1,6 @@
 from math import sqrt
 
-class _Vertex(object):
+class Vertex(object):
 
 	def __init__(self, coord_x, coord_y):
 		self.x = coord_x
@@ -10,10 +10,16 @@ class _Vertex(object):
 		deltax = self.x - other.x
 		deltay = self.y - other.y
 
-		return sqrt((deltax**deltax) + (deltay**deltay))
+		return sqrt((deltax**2) + (deltay**2))
 
 	def __str__(self):
 		return str(self.x)+' '+str(self.y)
+
+	def __repr__(self):
+		return 'V(' + str(self) + ')'
+
+	def __hash__(self):
+		return hash((self.x, self.y))
 
 
 class Graph(object):
@@ -40,25 +46,26 @@ class Graph(object):
 					else:
 						self.addEdge(vertex1, vertex2, 1)
 
-
 	def addVertex(self, vertex):
 		self.vertices.append(vertex)
+		self.edges[vertex] = {}
 
 	def addEdge(self, vertex1, vertex2, weight):
 		if (not ((vertex1 in self.vertices) and (vertex2 in self.vertices))):
 			raise ValueError("One of the vertices is not in the graph")
-		if (not vertex1 in self.edges):
-			self.edges[vertex1] = {}
+
 		self.edges[vertex1][vertex2] = weight
 
 	def getEdgeWeight(self, vertex1, vertex2):
-		if ((not vertex1 in self.edges) or (not vertex2 in self.edges[vertex1])):
+		if not vertex2 in self.edges[vertex1]:
 			raise ValueError("The edge does not exist")
+
 		return self.edges[vertex1][vertex2]
 
 	def hasEdge(self, vertex1, vertex2):
 		if ((not vertex1 in self.edges) or (not vertex2 in self.edges[vertex1])):
 			return False
+
 		return True
 
 	def dump(self, file_name):
@@ -66,3 +73,9 @@ class Graph(object):
 			for vertex1 in self.edges:
 				for vertex2 in self.edges[vertex1]:
 					file.write(str(vertex1)+'-'+str(vertex2))
+
+	def getAdjacents(self, vertex):
+		return self.edges[vertex]
+
+	def getVertices(self):
+		return self.vertices
