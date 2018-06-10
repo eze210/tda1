bd7cb379336314debeb37994466b7c9fa4908fcf
eze210 <eze210@gmail.com>
2018-06-09 23:58:25 -0300

    Adding file chooser.

--- tp3/main.py
+++ tp3/main.py
@@ -9,6 +9,15 @@ from kivy.uix.filechooser import FileChooser, FileChooserIconView
 from shipPlayer import *
 
 
+class StartButton(Button):
+
+    def __init__(self, **kwargs):
+        super(StartButton, self).__init__(**kwargs)
+
+    def on_press(self):
+        self.parent.startGame()
+
+
 class ShipButton(Button):
 
     def __init__(self, ship, game, **kwargs):
@@ -18,7 +27,7 @@ class ShipButton(Button):
 
     def on_press(self):
         super(ShipButton, self).on_press()
-        game.selectRow(self.ship.identifier)
+        self.game.selectRow(self.ship.identifier)
         self.parent.update()
 
 
@@ -71,31 +80,40 @@ class GridView(GridLayout):
             label.updateHealth()
 
 
+class MyFileChooser(FileChooserIconView):
+
+    def __init__(self, **kwargs):
+        super(MyFileChooser, self).__init__(**kwargs)
+        self.bind(selection=self.selectFile)
+
+    def selectFile(self, chooser, fileName):
+        self.parent.loadFile(fileName[0])
+
+
 class GameView(GridLayout):
 
-    def __init__(self, game, **kwargs):
+    def __init__(self, **kwargs):
         super(GameView, self).__init__(**kwargs)
-        self.gridView = GridView(game)
 
         self.cols = 1
+        self.chooser = MyFileChooser(path=".")
+        self.add_widget(self.chooser)
+
+    def loadFile(self, fileName):
+        print(fileName)
+        self.game = loadGame(fileName)
+        self.gridView = GridView(self.game)
+        self.remove_widget(self.chooser)
         self.add_widget(self.gridView)
 
 
 class GameApp(App):
 
-    def __init__(self, game):
-        self.game = game
+    def __init__(self):
         App.__init__(self)
 
     def build(self):
-        return GameView(self.game)
+        return GameView()
 
 if __name__ == '__main__':
-    if len(argv) < 2:
-        print("Uso: python {} <shipfile>".format(argv[0]))
-        exit(1)
-    fileName = argv[1]
-    print("Cargando {0}".format(fileName))
-    game = loadGame(fileName)
-
-    GameApp(game).run()
+    GameApp().run()
