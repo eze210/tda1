7e0b20d4a905c1576f0ca92713660e65742982e6
axl <voices117@gmail.com>
2018-06-18 01:08:09 -0300

    added some comments

--- tp3/initial_pos.py
+++ tp3/initial_pos.py
@@ -2,6 +2,9 @@ from dynamic import print_level, run_simulation, solve_game
 
 
 def left_shift(tup, n):
+    """Given a tuple `tup` and an integer `n`, shifts all elements in `tup` `n` places
+    to the left."""
+
     if not tup or not n:
         return tup
     n %= len(tup)
@@ -9,39 +12,52 @@ def left_shift(tup, n):
 
 
 def build_dmg_grid(dmg_grid, ship_positions):
+    """Given a damage grid and a list of ship initial positions, transforms `dmg_grid`
+    so as if the ships were to start at the first position shifting the rows."""
+
     new_grid = []
     for pos, row in zip(ship_positions, dmg_grid):
         new_grid.append(left_shift(row, pos))
     return new_grid
 
 
-def solve_positions(D, dmg_grid, hitpoints, num_shots, ship_positions):
+def solve_positions(D, dmg_grid, hitpoints, num_shots, ship_positions, solver):
+    """Returns the initial ship positions that maximize the score of the `solver`."""
+
+    # checks if this subproblem was already solved
     if ship_positions in D:
         return D[ship_positions]
 
     num_ships = len(hitpoints)
     num_cols = len(dmg_grid[0])
+
+    # base case: if all ships are positioned, solves the game
     if num_ships == len(ship_positions):
         dmg_grid = build_dmg_grid(dmg_grid, ship_positions)
-        points, _ = solve_game(dmg_grid, hitpoints, num_shots)
+        points, _ = solver(dmg_grid, hitpoints, num_shots)
         D[ship_positions] = points, ship_positions
     else:
         best_score, best_positions = 0, tuple()
-        for i in range(num_ships - len(ship_positions)):
-            for j in range(num_cols):
-                new_score, new_positions = solve_positions(D, dmg_grid, hitpoints, num_shots, ship_positions + tuple([j]))
-                if new_score > best_score:
-                    best_score, best_positions = new_score, new_positions
+
+        # goes through every possible position for the n'th ship and finds the position
+        # of the ships (not already positioned) that maximizes the score
+        for j in range(num_cols):
+            new_score, new_positions = solve_positions(D, dmg_grid, hitpoints, num_shots, ship_positions + tuple([j]), solver)
+            if new_score > best_score:
+                best_score, best_positions = new_score, new_positions
         D[ship_positions] = best_score, best_positions
     return D[ship_positions]
 
 
-def get_ship_positions(dmg_grid, hitpoints, num_shots):
-    """Finds the initial ship positions that would maximize the adversary's points."""
-
+def get_ship_positions(dmg_grid, hitpoints, num_shots, solver):
+    """Finds the initial ship positions that would maximize the adversary's points.
+    `dmg_grid` is the grid of damage points for each position.
+	`hitpoints` the ships HP.
+	`num_shots` the total number of shots allowed in a single turn.
+    `solver` an algorithm that solves the game and receives the 3 previous parameters.
+    """
     D = {}
-    return solve_positions(D, dmg_grid, hitpoints, num_shots, tuple())[1]
-
+    return solve_positions(D, dmg_grid, hitpoints, num_shots, tuple(), solver)[1]
 
 
 if __name__ == '__main__':
@@ -51,7 +67,7 @@ if __name__ == '__main__':
         [400, 400, 50, 100],
         [300, 200, 100, 300],
     ]
-    ships_health = [800, 800, 800]
+    ships_health = [700, 800, 400]
     print_level(ships_health, level)
     shots_per_turn = 4
 
@@ -63,10 +79,10 @@ if __name__ == '__main__':
     print()
 
     # finds the worst case
-    positions = get_ship_positions(level, ships_health, shots_per_turn)
-    new_grid = build_dmg_grid(level, positions)
-    print_level(ships_health, new_grid)
-    solution = solve_game(new_grid, ships_health, shots_per_turn)
+    positions = get_ship_positions(level, ships_health, shots_per_turn, solve_game)
+    new_level = build_dmg_grid(level, positions)
+    print_level(ships_health, new_level)
+    solution = solve_game(new_level, ships_health, shots_per_turn)
     print()
     print()
     print('ship positions')
@@ -77,4 +93,4 @@ if __name__ == '__main__':
     print()
 
     # runs and shows the game using the obtained solution
-    run_simulation(level, ships_health, solution[1])
+    run_simulation(new_level, ships_health, solution[1])
